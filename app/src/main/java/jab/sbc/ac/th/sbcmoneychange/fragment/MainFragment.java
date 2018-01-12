33a59661ad08087b4a87cc17c963e00aee34c451
jabgit <jabnuttawut@gmail.com>
2018-01-12 16:06:33 +0700

    Finish 12Jan

---
+++ app/src/main/java/jab/sbc/ac/th/sbcmoneychange/fragment/MainFragment.java
@@ -0,0 +1,28 @@
+package jab.sbc.ac.th.sbcmoneychange.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import jab.sbc.ac.th.sbcmoneychange.R;
+
+/**
+ * Created by LAB3201_00 on 12/1/2561.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return view;
+    }
+} //main Class
